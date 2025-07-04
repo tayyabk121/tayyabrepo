5d9b694fddf04f93b61766e987a8ae82bd74d023
tayyab <tayyabkhanbhopal@gmail.com>
2025-07-04 16:46:01 +0530

    files added

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Use OpenJDK 24 slim image
+FROM eclipse-temurin:24-jdk
+
+# Set working directory
+WORKDIR /app
+
+# Copy JAR file into the container
+COPY target/demo-0.0.1-SNAPSHOT.jar app.jar
+
+# Expose default Spring Boot port
+EXPOSE 8080
+
+# Run the Spring Boot application
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/main/java/com/tyb/demo/controller/NewController.java
@@ -0,0 +1,19 @@
+package com.tyb.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NewController {
+
+//    @GetMapping
+//    public ResponseEntity greeting(){
+//        return ResponseEntity.ok("Hellooooo");
+//    }
+    @GetMapping("/greet")
+    public ResponseEntity greet(){
+        return ResponseEntity.ok("its working");
+    }
+}
